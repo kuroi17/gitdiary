57486971947766b6858689798b0453f559a4ff12
Jyvhan Earl Ponce <140694543+kuroi17@users.noreply.github.com>
2025-11-25 09:35:02 +0000

    folder structure

---
+++ sql/schema.sql
@@ -0,0 +1,10 @@
+create database if not exists gitDiary;
+use gitDiary;
+
+
+create table entries (
+    entryNumber primary key int AUTO_INCREMENT,
+    entryTitle varchar(255) not null,
+    entryContent text not null,
+    createdAt timestamp default current_timestamp
+)
