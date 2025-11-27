ad9c1d1d12ac0f3c09870687f1e88ecd7f9fcad0
Jyvhan Earl Ponce <140694543+kuroi17@users.noreply.github.com>
2025-11-27 14:33:50 +0000

    make the html into dynamic by using js literal sting + localStorage and stringify and parse for object save

--- frontend/assets/js/entries.js
+++ frontend/assets/js/entries.js
@@ -30,8 +30,40 @@ document.addEventListener("DOMContentLoaded", () => {
         return;
       }
 
+      // entryData will only be saved in local storage if it is in string format
+
+      // retrieve existing entries from local storage or initialize an empty array
+      let entries = JSON.parse(localStorage.getItem("entries")) || [];
+      // add the new entry to the entries array
+      entries.push(entryData);
+      // save the updated entries array back to local storage
+      localStorage.setItem("entries", JSON.stringify(entries));
+
       alert("Entry submitted successfully!");
       console.log("Submitted Entry Data:", entryData);
+      window.location.href = "new-entry.html"; // refresh to entries page
     });
+  }
+
+  const entriesContainer = document.querySelector(".entriesContainer");
+
+  if (entriesContainer) {
+    const entries = JSON.parse(localStorage.getItem("entries")) || [];
+
+    let entriesContainerHTML = "";
+    entries.forEach((entryData) => {
+      entriesContainerHTML += `
+  <div class="entryCard border p-3 rounded mt-4 w-75 mx-auto">
+        <h3 class="fw-semibold">${entryData.title}</h3>
+        <span class="text-gray-500">${entryData.date}</span>
+        <p class="mt-3 text-gray-600">
+          ${entryData.content}
+          GitDiary application. Here you can read your past thoughts and
+          reflections.
+        </p>
+      </div>
+`;
+    });
+    entriesContainer.innerHTML = entriesContainerHTML;
   }
 });
