84083655c067e0ee2ad1b233d5e8b896ad7176ae
Jyvhan Earl Ponce <140694543+kuroi17@users.noreply.github.com>
2025-11-27 13:21:14 +0000

    initialize object keys and values as well as add consoloe.log + alert to check if it works

--- frontend/assets/js/entries.js
+++ frontend/assets/js/entries.js
@@ -1,7 +1,37 @@
 // use when functions is specific to entries page
 
-const buttonSelector = document.querySelector(".newEntriesBtn");
+document.addEventListener("DOMContentLoaded", () => {
+  const buttonSelector = document.querySelector(".newEntriesBtn");
+  if (buttonSelector) {
+    buttonSelector.addEventListener("click", () => {
+      window.location.href = "new-entry.html";
+    });
+  }
+  const submitEntryButton = document.querySelector(".jsButtonSubmitEntry");
 
-buttonSelector.addEventListener("click", () => {
-  window.location.href = "new-entry.html";
+  if (submitEntryButton) {
+    submitEntryButton.addEventListener("click", () => {
+      const title = document.querySelector(".jsTitleUserInput").value;
+      const content = document.querySelector(".jsContentUserInput").value;
+      const date = document.querySelector(".jsDateUserInput").value;
+      const image = document.querySelector(".jsImageUserInput").value;
+
+      const entryData = {
+        title: title,
+        content: content,
+        date: date,
+        image: image ? image : null, // if image then image as value else null
+      };
+
+      console.log(entryData);
+
+      if (!title || !content) {
+        alert("Pls put title and content");
+        return;
+      }
+
+      alert("Entry submitted successfully!");
+      console.log("Submitted Entry Data:", entryData);
+    });
+  }
 });
